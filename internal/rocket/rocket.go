e45e1e9e2d0bc8f9dce7c0fa50cf8a89470ea58f
saif-paywell <bd.saif321@gmail.com>
2023-02-11 22:52:35 +0600

    created basic rocket model

---
+++ internal/rocket/rocket.go
@@ -0,0 +1,54 @@
+package rocket
+
+import "context"
+
+// Rocket-should contain the definition of out rocket
+type Rocket struct {
+	ID      string
+	Name    string
+	Type    string
+	Flights int
+}
+
+// Store - Defines the interface we expect
+// Our database implementation to follow
+type Store interface {
+	GetRocketByID(id string) (Rocket, error)
+	InsertRocket(rkt Rocket) (Rocket, error)
+	DeleteRocket(id string) error
+}
+
+// Service - our rocket service is responsible for
+// updating the rocket
+type Service struct {
+	Store Store
+}
+
+// New - returns a new instance of our rocket service
+func New(store Store) Service {
+	return Service{
+		Store: store,
+	}
+}
+
+// GetRocketByID - retrives a rocket based on ID from the store
+func (s Service) GetRocketByID(ctx context.Context, id string) (Rocket, error) {
+	rkt, err := s.Store.GetRocketByID(id)
+
+	if err != nil {
+		return Rocket{}, err
+	}
+
+	return rkt, nil
+}
+
+// InsertRocket - used to insert a new rocket into the store
+func (s Service) InsertRocket(rkt Rocket) (Rocket, error) {
+	rkt, err := s.Store.InsertRocket(rkt)
+
+	if err != nil {
+		return Rocket{}, err
+	}
+
+	return rkt, nil
+}
